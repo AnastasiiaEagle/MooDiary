9620b077b44d6cdd9c75c4b13d696ce671d71be7
Dellari0n <anastasia.ea9le@gmail.com>
2025-05-13 12:28:04 +0300

    1

---
+++ server/src/main.ts
@@ -0,0 +1,14 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import * as cookieParser from 'cookie-parser'
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.use(cookieParser()) // підключення кукі
+  app.useGlobalPipes( new ValidationPipe()); // підключення валідатора
+
+  await app.listen(process.env.PORT ?? 3000);
+}
+bootstrap();
